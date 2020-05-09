0ff4728979e788a7325d906c50870e3ead58aac5
DetaliDigital <admin@detali.digital>
2020-05-09 23:36:17 +0300

    Изучаем как делать галерею на отдельной вкладке

--- infoBlock/assets/components/infoblock/js/mgr/widgets/items.windows.js
+++ infoBlock/assets/components/infoblock/js/mgr/widgets/items.windows.js
@@ -5,7 +5,7 @@ infoBlock.window.CreateItem = function (config) {
     }
     Ext.applyIf(config, {
         title: _('infoblock_item_create'),
-        width: 800,
+        width: 850,
         autoHeight: true,
         url: infoBlock.config.connector_url,
         action: 'mgr/item/create',
@@ -111,10 +111,11 @@ infoBlock.window.UpdateItem = function (config) {
     }
     Ext.applyIf(config, {
         title: _('infoblock_item_update'),
-        width: 800,
+        width: 850,
         autoHeight: true,
         url: infoBlock.config.connector_url,
         action: 'mgr/item/update',
+        bodyCssClass: 'tabs',
         fields: this.getFields(config),
         keys: [{
             key: Ext.EventObject.ENTER, shift: true, fn: function () {
@@ -127,86 +128,25 @@ infoBlock.window.UpdateItem = function (config) {
 
 };
 
-Ext.extend(infoBlock.window.UpdateItem, MODx.Window, {
+Ext.extend(infoBlock.window.UpdateItem, infoBlock.window.CreateItem, {
 
     getFields: function (config) {
         return [{
-            xtype: 'hidden',
-            name: 'id',
-            id: config.id + '-id',
-        }, {
-            layout: 'column',
+            xtype: 'modx-tabs',
             items: [{
-                columnWidth: .7,
+                title: _('infoblock_item_tabs_content'),
                 layout: 'form',
-                defaults: {msgTarget: 'under'},
-                items: [{
-                    xtype: 'textfield',
-                    fieldLabel: _('infoblock_item_name'),
-                    name: 'name',
-                    id: config.id + '-name',
-                    anchor: '99%',
-                    allowBlank: false,
-                }, {
-                    xtype: 'modx-combo-browser',
-                    fieldLabel: _('infoblock_item_image'),
-                    name: 'image',
-                    id: config.id + '-image',
-                    source: MODx.config.modextra_source_images || MODx.config.default_media_source,
-                    anchor: '99%',
-                    allowBlank: true,
-                }, {
-                    xtype: 'textarea',
-                    fieldLabel: _('infoblock_item_description'),
-                    name: 'description',
-                    id: config.id + '-description',
-                    anchor: '99%',
-                    height: 50,
-                }, {
-                    xtype: 'textarea',
-                    fieldLabel: _('infoblock_item_content'),
-                    name: 'content',
-                    id: config.id + '-content',
-                    height: 200,
-                    anchor: '99%',
-                } , {
-        						xtype: 'infoblock-filter-resources',
-        	          fieldLabel: _('infoblock_item_url'),
-        						name: 'url',
-                    id: config.id + '-url',
-        						anchor: '99%',
-        						allowBlank: true,
-        				}],
+                items: infoBlock.window.CreateItem.prototype.getFields.call(this, config),
             }, {
-                columnWidth: .3,
-                layout: 'form',
+                title: _('infoblock_item_tabs_gallery'),
                 items: [{
-                    xtype: 'infoblock-combo-position',
-                    fieldLabel: _('infoblock_item_position'),
-                    anchor: '99%',
-                    allowBlank: true,
-                }, {
-                    xtype: 'infoblock-combo-dates',
-                    fieldLabel: _('infoblock_item_createdon'),
-                    name: 'createdon',
-                    id: config.id + '-createdon',
-                    anchor: '99%',
-                    allowBlank: true,
-                }, {
-                    xtype: 'xcheckbox',
-                    boxLabel: _('infoblock_item_active'),
-                    name: 'active',
-                    id: config.id + '-active',
-                }],
+                    xtype: 'infoblock-panel-gallery',
+                    record: config.record,
+                }]
             }]
-        },
-        ];
-    },
-
-    loadDropZones: function () {
+        }];
     }
 
 });
-
 
 Ext.reg('infoblock-item-window-update', infoBlock.window.UpdateItem);
