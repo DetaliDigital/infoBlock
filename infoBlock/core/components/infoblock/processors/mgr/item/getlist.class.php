f6cd7c1e55da020e1ceaf4e73d3160541d798b62
DetaliDigital <admin@detali.digital>
2020-06-01 02:23:08 +0300

    Добавил возможность поиска по столбцу "Позиция"

--- infoBlock/core/components/infoblock/processors/mgr/item/getlist.class.php
+++ infoBlock/core/components/infoblock/processors/mgr/item/getlist.class.php
@@ -35,13 +35,16 @@ class infoBlockItemGetListProcessor extends modObjectGetListProcessor
         $query = trim($this->getProperty('query'));
 
         $c->leftJoin('infoBlockPosition', 'infoBlockPosition', 'infoBlockPosition.id = infoBlockItem.position_id');
-        $c->select(array($this->modx->getSelectColumns('infoBlockItem', 'infoBlockItem')));
+        $c->select(array(
+            $this->modx->getSelectColumns('infoBlockItem', 'infoBlockItem')
+            ));
         $c->select(['infoBlockPosition.name as position_name']);
 
         if ($query) {
             $c->where([
                 'name:LIKE' => "%{$query}%",
                 'OR:description:LIKE' => "%{$query}%",
+                'OR:infoBlockPosition.name:LIKE' => "%{$query}%",
             ]);
         }
 
